18d191343a1d0023504ab96c4eaf055a4e1c5716
showlist <wys10162006@126.com>
2020-01-16 21:13:13 +0800

    add the github webhook

--- client-service1/src/main/java/com/demo/utils/PageUtil.java
+++ client-service1/src/main/java/com/demo/utils/PageUtil.java
@@ -1,15 +1,14 @@
-package com.demo.utils;
-
-import java.util.List;
-import java.util.function.Function;
-
-import com.github.pagehelper.Page;
-import com.github.pagehelper.PageHelper;
-
-public class PageUtil<E>{
-
-	public Page<E> executePage(PageRequest<E> request, Function<E, List<E>> fun){
-		return PageHelper.startPage(request.getPageNo(), request.getPageSize(), request.getOrderBy())
-				  .doSelectPage(() -> fun.apply(request.getT()));
-	}
-}
+/*
+ * package com.demo.utils;
+ * 
+ * import java.util.List; import java.util.function.Function;
+ * 
+ * import com.github.pagehelper.Page; import com.github.pagehelper.PageHelper;
+ * 
+ * public class PageUtil{
+ * 
+ * public static <T> Page<T> executePage(PageRequest<T> request, Function<T,
+ * List<T>> fun){ return PageHelper.startPage(request.getPageNo(),
+ * request.getPageSize(), request.getOrderBy()) .doSelectPage(() ->
+ * fun.apply(request.getT())); } }
+ */
--- client-service2/src/main/java/com/demo/controller/TestController.java
+++ client-service2/src/main/java/com/demo/controller/TestController.java
@@ -2,12 +2,14 @@ package com.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.service.remote.RemoteService;
 
 @RestController
+@RefreshScope
 public class TestController {
 
 	@Autowired
